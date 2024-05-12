5d962ff2da2490cdef9b156379d72df980c6e5ca
Praballl <116371043+praballl@users.noreply.github.com>
2024-05-12 12:39:36 +0530

    created the pages for the application

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 
 export default function App() {
   return (
-    <div className='text-slate-400 text-3xl'>
-      App
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ clint/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div className='bg-slate-600'>
+      signin
+    </div>
+  )
+}
+
+export default SignIn
